cc82b38d199432eb68af731b1b3c1305bdc1de88
ManongJu <469863752@qq.com>
2016-06-01 12:46:25 +0800

    add bugly

--- app/src/main/java/com/github/yinyuetai/YinYueApplication.java
+++ app/src/main/java/com/github/yinyuetai/YinYueApplication.java
@@ -2,6 +2,8 @@ package com.github.yinyuetai;
 
 import android.app.Application;
 
+import com.tencent.bugly.crashreport.CrashReport;
+
 
 /**
  * Created by Mr.Yangxiufeng on 2016/5/10.
@@ -10,5 +12,6 @@ public class YinYueApplication extends Application{
     @Override
     public void onCreate() {
         super.onCreate();
+        CrashReport.initCrashReport(getApplicationContext());
     }
 }
